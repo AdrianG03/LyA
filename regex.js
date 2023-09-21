1c6c54b985dcc0e7a2245a3ccbc72705b9205317
AdrianG <guevadev@gmail.com>
2023-09-20 18:40:30 -0600

    Programa venezuela

--- regex.js
+++ regex.js
@@ -1,4 +1,25 @@
-let txt = "E-192836a4"
 const patron = /^(V|E)-\d{1,8}$/
 
-console.log(patron.test(txt));
+function obtener() {
+    let txt = document.getElementById("caja").value
+    let res = patron.test(txt)
+
+    if (res == true) {
+        if (txt.substring(0, 1) == "V") {
+            alert("Cédula válida, es Venezolano")
+        } else {
+            alert("Cédula válida, es Extranjero")
+        }
+    } else {
+        if (txt.substring(0, 1) == "") {
+            alert("Ingrese datos primero")
+        } else {
+            alert("Cédula inválida")
+        }
+    }
+}
+
+
+
+
+
